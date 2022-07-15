6fad138d776bfd971ffd163bcdac0fd0b315f84e
anarakinson <anarakinson@gmail.com>
2022-07-15 17:31:50 +0300

    add contains int func

--- utils.go
+++ utils.go
@@ -8,3 +8,12 @@ func Contains(array []string, target string) bool {
     }
     return false
 }
+
+func ContainsInt(array []int, target int) bool {
+    for _, el := range(array) {
+        if (el == target) {
+            return true
+        }
+    }
+    return false
+}
